49163c6ac603cc6f6f83d665338f80b0328ba575
Kyle Haver <kyle.haver@slalom.com>
2016-02-22 10:40:49 -0600

    Reduced refresh time to 5 minutes

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var myApp = angular.module('app', []);
 myApp.controller('MainController', ['$scope','$http', '$interval', function($scope, $http, $interval){
     var vm = this;
     $scope.title = 'Status Board';
-    
+
     var updateStatuses = function() {
       $http.get('http://khaver.mynetgear.com:3000/Statuses').success(function(data, status, headers, config) {
         console.log("Response: " + JSON.stringify(data));
@@ -11,5 +11,5 @@ myApp.controller('MainController', ['$scope','$http', '$interval', function($sco
       })
     };
 //    updateStatuses()
-    $interval(updateStatuses, 3000);
+    $interval(updateStatuses, 300000);
 }]);
